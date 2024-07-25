0c17a9916d2efa46ecd9b73f9a754f0ae5d2d21b
Hepipipi <saltanat010909@gmail.com>
2024-07-25 15:24:50 +0600

    update

--- sa.py
+++ sa.py
@@ -1,4 +1,22 @@
-print('Hello')
+print('start doing homework')
+
+
+def my_decorator(func):
+    def start(num):
+        print('start')
+        res = func(num)
+        print(num + 2)  # Печатаем значение num + 2
+        print('end')
+        return res  # Возвращаем результат функции
+    return start
+
+num1 = int(input('enter any num '))
+
+@my_decorator
+def example(num1):
+    return num1
+
+example(num1)
 
 
 
